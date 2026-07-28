c9f7cc204e65e4e400f21512beb6040bdbf743ea
maksymosypchuk <maksym3d@gmail.com>
2026-07-28 21:11:04 +0300

    update (main.ts): update app.enableCors()

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+      origin: true,
+      credentials: true,
+  });
   app.setGlobalPrefix("api");
   app.useGlobalPipes(
       new ValidationPipe({
